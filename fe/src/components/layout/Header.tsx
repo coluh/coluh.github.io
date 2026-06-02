94deb00ce1b4496fe311e922d02020dc3704966a
coluh <coluh.b@gmail.com>
2026-06-02 10:10:33 +0800

    style: optimize header layout on mobile

--- fe/src/components/layout/Header.tsx
+++ fe/src/components/layout/Header.tsx
@@ -14,14 +14,14 @@ export default function Header() {
   const location = useLocation();
 
   return (
-    <header className="flex flex-row gap-2 bg-pink-200 p-2 transition-colors md:w-32 md:flex-col md:p-4 dark:bg-purple-700 dark:text-gray-200">
+    <header className="flex flex-row items-center gap-2 bg-pink-200 p-2 transition-colors md:w-32 md:flex-col md:items-stretch md:p-4 dark:bg-purple-700 dark:text-gray-200">
       <Avatar className="size-14 text-blue-400 md:mx-auto md:my-2 md:size-16" />
       <nav className="flex flex-row gap-2 rounded-lg bg-black/10 p-2 md:flex-col dark:bg-white/20">
         {navItems.map((item) => (
           <Link
             key={item.to}
             to={item.to}
-            className={`rounded-lg p-2 text-center hover:text-blue-500 dark:hover:text-blue-300 ${location.pathname === item.to ? "bg-white/50 dark:bg-black/40" : ""}`}
+            className={`rounded-lg px-2 py-1 text-center hover:text-blue-500 dark:hover:text-blue-300 ${location.pathname === item.to ? "bg-white/50 dark:bg-black/40" : ""}`}
           >
             {item.label}
           </Link>
